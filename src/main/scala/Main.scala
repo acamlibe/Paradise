2474ec13cd65a3271079f8f839535be455be0e76
Ali Camlibel <acamlibe@uncc.edu>
2024-03-31 00:47:57 -0400

    Added text input loop

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,17 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import scala.io.StdIn.readLine
 
-def msg = "I was compiled by Scala 3. :)"
+@main def hello(): Unit =
+  gameLoop()
+
+def gameLoop(): Unit = {
+  val input = getInput()
+} 
+
+def getInput(): String =
+  print("> ")
+  val input = readLine()
+  if (isNotEmpty(input)) input.toLowerCase()
+  else getInput()
+
+def isNotEmpty(s: String) = 
+  Option(s).map(_.trim).exists(_.nonEmpty)
