bda9f7574add66bad945beba1ff05a56a9a3e5a1
Ali Camlibel <acamlibe@uncc.edu>
2024-04-06 03:12:32 -0400

    Started story

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,33 @@
 import scala.io.StdIn.readLine
+import scala.concurrent.duration._
 
 @main def hello(): Unit =
   gameLoop()
 
 def gameLoop(): Unit = {
+
+  printTitle()
+
+  printStoryMessage("The year is 2048...")
+
+  printStoryMessage("Paradise, formerly known as London, is one of the few remaining " +
+    "major cities left in the country where " +
+    "the middle and upper classes reside. " +
+    "It's towering structures and state-of-the-art infrastructure symbolize the pinnacle of " +
+    "human development. Technology seamlessly integrates into every aspect of life, " +
+    "creating a world where everything is efficient, convenient, and advanced. " +
+    "Humans have incorporated cybernetic enhancements into their bodies, " +
+    "blurring the lines between organic and synthetic and elevating their abilities to new heights.")
+
+  printStoryMessage("Your name is Alex Mercer, and you're just another face in the crowd of Paradise. " +
+    "You live in a cramped, rundown apartment in one of the city's less desirable districts. " +
+    "The walls are thin, the plumbing is questionable, and the neon lights from the street outside " +
+    "never quite let the darkness settle in. Each day blends into the next as you go through the motions, " +
+    "working a job that pays the bills but offers little else. " +
+    "In the midst of the technological marvel that is Paradise, you're just another soul trying to make it through another day. ")
+
+  printStoryMessage("You wake up in your apartment Sunday morning. ")
+
   val input = getInput()
 
   input match {
@@ -13,6 +37,26 @@ def gameLoop(): Unit = {
   }
 
   gameLoop()
+}
+
+def printTitle(): Unit = {
+  val title = 
+    """
+       _____  _______  ______ _______ ______  _____ _______ _______
+      |_____] |_____| |_____/ |_____| |     \   |   |______ |______
+      |       |     | |    \_ |     | |_____/ __|__ ______| |______
+    """
+  println(title)
+}
+
+def printStoryMessage(message: String): Unit = {
+  message.foreach { char =>
+    print(char)
+    Thread.sleep(25)
+  }
+  println()
+  println()
+  Thread.sleep(1000)
 }
 
 def printHelp(): Unit = {
